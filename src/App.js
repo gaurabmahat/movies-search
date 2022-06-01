5ad83ddc82da3947a00ae88a71a923712f530884
gaurabmahat <gaurab.mahat.work@gmail.com>
2022-06-01 13:49:37 +0300

    added an input field and button to the navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
+import Navbars from './Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Movies
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbars />
+      <h1>Movies</h1>
     </div>
   );
 }
