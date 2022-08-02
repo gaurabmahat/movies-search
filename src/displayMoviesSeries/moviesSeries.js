ac9ce680be26bf701813c0f3186212944d71db83
gaurabmahat <gaurab.mahat.work@gmail.com>
2022-08-02 11:20:02 +0300

    removed unused-vars

--- src/displayMoviesSeries/moviesSeries.js
+++ src/displayMoviesSeries/moviesSeries.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { MoviesArray, SeriesArray } from "../Helper/UserInputContext";
 
